9b94959cd79e6e210166ce147398cd530f1278c5
Duberly Guarnizo Fuentes Rivera <duberlygfr@gmail.com>
2021-05-23 13:05:49 -0500

    Initial commit, just for fun. This is supposed to be a generic static queue for educational purposes, made by Duberly Guarnizo, while coursing Software Engineering at the UTP.

---
+++ clasessemestre/src/semananueve/CustomStaticQueue.java
@@ -0,0 +1,9 @@
+package semananueve;
+
+public class CustomStaticQueue<T> {
+	private T[] contenedor;
+
+	public CustomStaticQueue(int size) {
+		// this.contenedor = new T[size];
+	}
+}
---
+++ clasessemestre/src/semananueve/CustomStaticQueueTest.java
@@ -0,0 +1,14 @@
+package semananueve;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CustomStaticQueueTest {
+
+	@Test
+	void testCustomStaticQueue() {
+		fail("Not yet implemented");
+	}
+
+}
